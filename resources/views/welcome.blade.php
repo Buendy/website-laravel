2111cbf49a53a179de3f3ed75140bd5c1c4c73ab
root <Buendy>
2018-11-30 16:14:15 +0000

    Modificación titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Website de la muerte
                 </div>
 
                 <div class="links">
